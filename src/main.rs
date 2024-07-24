1f55cf3dda6ed14fd851e6ce75f01eb75666e5b7
lucas-bernardino <lucasgber@gmail.com>
2024-07-24 19:50:00 -0300

    Add better error handling and clean code by adding function to deal with stream data

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,30 @@ async fn main() {
         Ok(urls) => urls,
         Err(e) => panic!("Failed to parse videos.\nCause: {e}"),
     };
-    let urls_len = urls.len();
-    let mut stream_vec: Vec<u8> = vec![];
-    for (index, url) in urls.iter().enumerate().take(10) {
-        println!("Saving url stream... [{index}/{urls_len}]");
-        let stream = reqwest::get(url).await.unwrap().bytes().await.unwrap();
-        stream_vec.append(&mut stream.as_ref().to_vec());
-    }
+    let stream_vec = match create_vector_of_streams(&urls).await {
+        Ok(stream_vec) => stream_vec,
+        Err(e) => panic!("Failed getting stream from the http request.\nCause: {e}"),
+    };
     let f = tokio::fs::File::create("video.mp4").await.unwrap();
     let mut writer = BufWriter::new(f);
     println!("Writing to file: ");
     let bytes_written = writer.write(stream_vec.as_ref()).await.unwrap() as f32;
     let bytes_written = bytes_written / ((1024 * 1000) as f32);
     println!("Successfully wrote {} MB to the file", (bytes_written));
+}
+
+async fn create_vector_of_streams(urls: &Vec<String>) -> Result<Vec<u8>, reqwest::Error> {
+    let mut stream_vec: Vec<u8> = vec![];
+    let client = reqwest::Client::new();
+    let urls_len = urls.len();
+
+    for (index, url) in urls.iter().enumerate().take(10) {
+        println!("Saving url stream... [{index}/{urls_len}]");
+        let stream = client.get(url.to_string()).send().await?.bytes().await?;
+        stream_vec.append(&mut stream.as_ref().to_vec());
+    }
+
+    Ok(stream_vec)
 }
 
 fn get_urls_from_file(path: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
